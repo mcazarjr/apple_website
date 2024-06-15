cc2e2458ccf6a0a33cb75836f106d4d3161139de
mcazarjr <meraldocazar@gmail.com>
2024-06-15 11:10:51 -0700

    chore: add iphone video and animation using gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,55 @@
+import { useEffect, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
+
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 768 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 768) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 1.5 });
+  });
+
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">iPhone 15 Pro</p>
+        <p
+          id="hero"
+          className="hero-title"
+        >
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+            className="pointer-events-none"
+          >
+            <source
+              src={videoSrc}
+              type="video/mp4"
+            ></source>
+          </video>
+        </div>
       </div>
     </section>
   );
